fb16f99f2a047e4bf60bd72905c10fa1f7db41bb
hzzheng <zhchaozju@gmail.com>
2018-04-24 23:36:42 +0800

    chore: add router

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const Koa = require("koa");
+const router = require("./server/routes");
 
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = "hello, world";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(4000);
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,11 @@ const router = new Router({
 });
 
 router.get("/:type/tasks", ctx => {
-  ctx.body = {};
+  ctx.body = `${ctx.params.type}/tasklist`;
 });
+
+router.get("/:type/task/:id", ctx => {
+  ctx.body = `${ctx.params.type}/task/${ctx.params.id}`;
+});
+
+module.exports = router;
